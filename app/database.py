0d1dfd2f97670b02e09548fb6467416a4a7c31b4
Frank Kreuwel <frank.kreuwel@alliander.com>
2022-02-09 08:57:17 +0100

    add examples for manual input

--- app/database.py
+++ app/database.py
@@ -12,11 +12,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-## setup the correct table
-# train_model_tasks(id, training_done, created, duration, trained_model_id, training_failed, reason_failed,
-#                   new_model_better)
-
-
 def db_model_as_dict(obj):
     return {
         c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs
